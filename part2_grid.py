66f8863c81308cbcc39380211aa4bbb1328c141f
eagles284 <eagles284@users.noreply.github.com>
2018-11-10 23:25:28 +0700

    Working Breadth-First Algo Visualizer
    OpenCV!!!

--- part2_grid.py
+++ part2_grid.py
@@ -2,27 +2,43 @@ import cv2
 import time
 import collections
 
-# map_bg = cv2.imread('map.png')
-# map_bg = cv2.cvtColor(map_bg, cv2.COLOR_BGR2GRAY)
-# map_editable = cv2.imread('map.png')
-# map_editable = cv2.cvtColor(map_editable, cv2.COLOR_BGR2GRAY)
+map_bg = cv2.imread('map.png')
+map_bg = cv2.cvtColor(map_bg, cv2.COLOR_BGR2GRAY)
+map_editable = cv2.imread('map.png')
+map_editable = cv2.cvtColor(map_editable, cv2.COLOR_BGR2GRAY)
 
-# startTime = time.time()
+startTime = time.time()
 
-# def showPreview():
-#     preview = cv2.resize(map_editable, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
-#     cv2.imshow('Mapping', preview)
+def showPreview():
+    preview = cv2.resize(map_editable, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST)
+    cv2.imshow('Mapping', preview)
 
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#     time.sleep(0.1)
+    if (cv2.waitKey(1) & 0xFF == ord('q')):
+        cv2.destroyAllWindows()
 
-class SimpleGraph:
-    def __init__(self):
-        self.edges = {}
+# (Y, X) Format !!!
+class SquareGrid:
+    def __init__(self, image):
+        self.img = image
+        self.width = len(image[0]) - 1
+        self.height = len(image) - 1
+    
+    def in_bounds(self, id):
+        (y, x) = id
+        return 0 <= x < self.width and 0 <= y < self.height
+    
+    def passable(self, id):
+        return self.img[id[0]][id[1]] <= 26
     
     def neighbors(self, id):
-        return self.edges[id]
+        (x, y) = id
+        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
+        if (x+y) % 2 == 0:
+            results = list(reversed(results))
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
 class Queue:
     def __init__(self):
         self.elements = collections.deque()
@@ -39,35 +55,32 @@ class Queue:
 
 # Workspace
 
-graf = SimpleGraph()
-graf.edges = {'Arya': ['Faiz'],
-              'Faiz': ['Dhika', 'Aqqil', 'Sigit', 'Aziz', 'Arya', 'Faisal'],
-              'Faisal': ['Rendy', 'Salman', 'Aril', 'Arya'],
-              'Aqqil': ['Alvis', 'Dhika', 'Faiz'],
-              'Dhika': ['Sigit', 'Aziz'],
-              'Rendy': ['Salman'],
-              'Salman': ['Rendy', 'Faisal'],
-              'Sigit': ['Aziz'],
-              'Aziz': ['Sigit'],
-              'Alvis': ['Aqqil', 'Arya', 'Faisal'],
-              'Aril': ['Sigit']}
+map_ml = SquareGrid(map_bg)
 
-def visit(graph, start):
-    visited = {}
-    visited[start] = True
+def breadth_first(grid, start):
+    came_from = {}
+    came_from[start] = None
+
     toVisit = Queue()
     toVisit.put(start)
 
     while not toVisit.empty():
-        currentPeople = toVisit.get()
+        currentPos = toVisit.get()
+        (y, x) = currentPos
+        map_editable[y][x] = 25
 
-        for neighbor in graf.neighbors(currentPeople):
-            if neighbor not in visited:
-                print('Visiting %r' % neighbor)
-                visited[neighbor] = True
+        for neighbor in map_ml.neighbors(currentPos):
+
+            if neighbor not in came_from:
+                came_from[neighbor] = currentPos
                 toVisit.put(neighbor)
 
-visit(graf, 'Arya')
+                # print('Visiting %r' % neighbor)
+                showPreview()
+    
+    return came_from
+
+breadth_first(map_ml, (136, 8))
 
 
 
